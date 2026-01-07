1f23e68a5f91f681c96e97c6df2c94dc6aa8a2cf
Stack64 <user.stack64@gmail.com>
2026-01-07 23:53:56 +0530

    Phase 1

---
+++ Phase1/SortingAlgos/S1.java
@@ -0,0 +1,31 @@
+package Phase1.SortingAlgos;
+
+public class S1 {
+
+    static void selectionSort(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n-1;i++) {
+            int minIndex=i;
+            for(int j=i+1;j<n;j++) {
+                if(arr[j]<arr[minIndex]) {
+                    minIndex=j;
+                }
+            }
+            int temp=arr[minIndex];
+            arr[minIndex]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    static void printArray(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr={64,25,12,22,11};
+        selectionSort(arr);
+        printArray(arr);
+    }
+}
